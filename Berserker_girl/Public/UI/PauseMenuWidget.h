43f0d5d750377474061af13a4b2aa1595869b293
ahnahn <80297360+ahnahn@users.noreply.github.com>
2025-08-21 04:31:15 +0900

    Create PauseMenuWidget.h

---
+++ Berserker_girl/Public/UI/PauseMenuWidget.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseMenuWidget.generated.h"
+
+class UButton;
+class USoundBase;
+
+UCLASS()
+class BERSERKER_GIRL_API UPauseMenuWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+protected:
+    virtual void NativeConstruct() override;
+
+    UPROPERTY(meta = (BindWidget))
+        UButton* Custom_Resume;
+
+    UPROPERTY(meta = (BindWidget))
+        UButton* Custom_Main;
+
+    UPROPERTY(meta = (BindWidget))
+        UButton* Custom_System;
+
+    UPROPERTY(meta = (BindWidget))
+        UButton* Custom_Quit;
+
+    UFUNCTION()
+        void OnResumeClicked();
+
+    UFUNCTION()
+        void OnMainClicked();
+
+    UFUNCTION()
+        void OnSystemClicked();
+
+    UFUNCTION()
+        void OnQuitClicked();
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI SFX")
+        USoundBase* ClickSound;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI Widgets")
+        TSubclassOf<UUserWidget> OptionsWidgetClass;
+
+private:
+    void OpenMainMenu();
+    void QuitTheGame();
+};
