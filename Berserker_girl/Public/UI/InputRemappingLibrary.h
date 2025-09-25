2f92910841d4b00a9e922621b542d1e70640f07b
ahnahn <80297360+ahnahn@users.noreply.github.com>
2025-09-26 05:29:15 +0900

    Create InputRemappingLibrary.h

---
+++ Berserker_girl/Public/UI/InputRemappingLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "InputRemappingLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BERSERKER_GIRL_API UInputRemappingLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
